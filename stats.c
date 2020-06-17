a1713e0ca73e9d8aed72a4e4f117a7e89bcca365
JOSE CORONACION SEDANO <jose.coronacion.s@uni.pe>
2020-06-17 11:17:14 -0600

    Adding the statistical functions, also checked the proper functioning of these

--- stats.c
+++ stats.c
@@ -25,22 +25,151 @@
 
 /* Size of the Data Set */
 #define SIZE (40)
-void print_statistics(unsigned char array);
-void print_array(unsigned char array);
+void print_statistics(unsigned char* ptr, unsigned int count);
+void print_array(unsigned char * ptr, unsigned int count);
 unsigned char find_median(unsigned char * ptr, unsigned int count);
 unsigned char find_mean(unsigned char * ptr, unsigned int count);
 unsigned char find_maximum(unsigned char * ptr, unsigned int count);
 unsigned char find_minimum(unsigned char * ptr, unsigned int count);
-unsigned char sort_array(unsigned char * ptr, unsigned int count);
-void main() {
+void sort_array(unsigned char * ptr, unsigned int count); 
 
+void main() {
+  /*test variables are added a, b, c ,d*/ 	
+  unsigned char a;
+  unsigned char b;
+  unsigned char c;
+  unsigned char d;	
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
+  c=find_mean(test,SIZE); 
+ 	
+  a=find_maximum(test,SIZE);
+  b=find_minimum(test,SIZE);
+  
+  d=find_median(test,SIZE);
+/*the statics are printed in order to check the code*/
+  printf(" mean: %u \n maximum: %u \n minimum: %u \n median: %u\n",c,a,b,d);
   
 
+  /*print_array(test,SIZE);
+  	
+	
+  sort_array(test,SIZE);
+	
+  print_array(test,SIZE);*/
+  
+
+}
+
+void print_statistics(unsigned char* ptr, unsigned int count){
+			
+}
+void print_array(unsigned char * ptr, unsigned int count){
+
+}
+unsigned char find_median(unsigned char * ptr, unsigned int count) {
+	unsigned char median;
+	unsigned char maxtemp;
+	unsigned char i;
+	unsigned char j;
+	unsigned char *tempptr;
+	unsigned char testa[count];
+	unsigned char test1[count];
+	unsigned char *test11=&test1[0];
+	unsigned char *ptr1=&testa[0];
+	for(i=0; i<count;i++){
+	*ptr1=*ptr;
+	ptr1++;
+	ptr++;	
+	}
+	ptr1-=count;
+	ptr-=count;
+	for(i=0; i<count;i++){
+		maxtemp=0;
+		for(j=0;j<count;j++){
+			if(*ptr1>maxtemp){
+				maxtemp=*ptr1;
+				tempptr=ptr1;
+				
+				}
+			ptr1++;
+			}
+			*test11=maxtemp;
+			
+			*tempptr=0;
+			ptr1-=count;
+			test11++;
+			}
+		test11-=count;
+	
+	if ((count%2)==1){
+		printf(" \n %u \n",*(test11+(count/2)));
+		median=*(test11+(count/2));			
+		}
+	else{
+		
+		median=(*(test11+(count/2))+*(test11+(count/2)-1))/2;		
+		}
+	return median;
+
+}
+unsigned char find_mean(unsigned char * ptr, unsigned int count){
+	unsigned short summation=0;
+	unsigned char i;
+		if (ptr==NULL){
+			return 0;
+			}
+		if (count<=0){
+			printf("\nThe value of the array size is not valid!\n");
+			return 0;		
+			}
+		for(i=0;i<count;i++){
+			summation+=(unsigned short)*ptr;
+			ptr++;	
+		}
+		return ((unsigned char)(summation/count));
+}
+unsigned char find_maximum(unsigned char * ptr, unsigned int count){
+	unsigned char max;
+	unsigned char i;
+		if (ptr==NULL){
+			return 0;
+			}
+		if (count<=0){
+			printf("\nThe value of the array size is not valid!\n");
+			return 0;		
+			}
+		max=*ptr;
+		for(i=0;i<count;i++){
+			if(*ptr>max){
+				max=*ptr;}
+			ptr++;
+		}
+		return max;
+
+}
+unsigned char find_minimum(unsigned char * ptr, unsigned int count){
+	unsigned char min;
+	unsigned char i;
+		if (ptr==NULL){
+			return 0;
+			}
+		if (count<=0){
+			printf("\nThe value of the array size is not valid!\n");
+			return 0;		
+			}
+		min=*ptr;
+		for(i=0;i<count;i++){
+			if(*ptr<min){
+				min=*ptr;}
+			ptr++;
+		}
+		return min;
+}
+void sort_array(unsigned char * ptr, unsigned int count){
+		
 }
 
--- stats.h
+++ stats.h
@@ -49,10 +49,9 @@ void print_array(unsigned char * ptr, unsigned int count);
  *  
  *  <This function uses pointers to reference the internal values of the array, then it creates a 
  *  copy of the array so that it can be manipulated without changing the values of the original
- *  array, finally by means of a loop it stores the highest value of the array, stores it in
- *  another array, and finally it deletes the value of the original array to look for the next 
- *  highest value. Once we obtain the ordered data, we evaluate whether the amount of data is 
- *  odd or even and determine the center value or the average of the center values.>
+ *  array, after that the function is going to order the array. Once we obtain the ordered data,
+ *  we evaluate whether the amount of data is  odd or even and determine the center value, the
+ *  center value depends of the parity of the number of elements.>
 .*
  *
  *  @param <unsigned char * ptr> <The pointer to a data set>
@@ -67,9 +66,9 @@ unsigned char find_median(unsigned char * ptr, unsigned int count);
  *  @brief   <Given an array of data and a length, returns the mean>
  *  
  *  <This function uses pointers to reference the internal values of the array, using the
- *  internal values and the amount of data makes a sum through a loop, the sum's type variable is 
- *  int because is greater than 256, once obtained the result of the sum is divided by the amount
- *  of data obtaining the mean value.>
+ *  internal values and the amount of data makes a sum through a for loop, the sum's type 
+ *  variable is int because is greater than 256, once obtained the result of the sum is divided
+ *  by the amount of data obtaining the mean value.>
  *
  *  @param <unsigned char * ptr> <The pointer to a data set>
  *  @param <unsigned int count> <The number of items of the array>
@@ -83,7 +82,8 @@ unsigned char find_mean(unsigned char * ptr, unsigned int count);
  *  @brief   <Given an array of data and a length, returns the maximum value>
  *  
  *  <This function uses pointers to reference the internal values of the array, using the
- *  internal values and the amount of data,through a loop compares and obtains the maximum value.>
+ *  internal values and the amount of data,through a for loop compares and obtains the 
+ *  maximum value.>
  *
  *  @param <unsigned char * ptr> <The pointer to a data set>
  *  @param <unsigned int count> <The number of items of the array>
@@ -96,7 +96,8 @@ unsigned char find_maximum(unsigned char * ptr, unsigned int count);
  *  @brief   <Given an array of data and a length, returns the minimum>
  *  
  *  <This function uses pointers to reference the internal values of the array, using the
- *  internal values and the amount of data,through a loop compares and obtains the minimun value.>
+ *  internal values and the amount of data,through a for loop compares and obtains the minimun
+ *   value.>
  *
  *  @param <unsigned char * ptr> <The pointer to a data set>
  *  @param <unsigned int count> <The number of items of the array>
@@ -108,15 +109,15 @@ unsigned char find_minimum(unsigned char * ptr, unsigned int count);
  *  @funtion <sort_array>
  *  @brief   <Given an array of data and a length, sorts the array from largest to smallest.>
  *  
- *  <This function uses pointers to reference the internal values of the array, Then it creates
+ *  <This function uses pointers to reference the internal values of the array, then it creates
  *  a copy of the array so that it can be manipulated without changing the values of the 
- *  original array, finally by means of a loop it stores the highest value of the array, 
+ *  original array, finally by means of a for loop it stores the highest value of the array, 
  *  stores it in another array, and finally it deletes the value of the original array to
- *  look for the next highest value. Once we obtain the ordered data copy these values to 
- *  the original array.>
+ *  look for the next highest value and funcion do it for all elments. Once we obtain the 
+ *  ordered data copy these values to the original array.>
  *
  *  @param <unsigned char * ptr> <The pointer to a data set>
- *         <unsigned int count> <The number of items of the array>
+ *  @param <unsigned int count> <The number of items of the array>
  */ 
 void sort_array(unsigned char * ptr, unsigned int count); 
 
